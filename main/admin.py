8b913beb251afad00096c645ec1747ef91c0c115
GorHovsepyann <146917478+GorHovsepyann@users.noreply.github.com>
2024-02-22 20:22:25 +0400

    v2

---
+++ main/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Header,Etap1,Choice,Product,About,Advantages
+
+# Register your models here.
+admin.site.register(Header)
+admin.site.register(Etap1)
+admin.site.register(Choice)
+admin.site.register(Product)
+admin.site.register(About)
+admin.site.register(Advantages)
